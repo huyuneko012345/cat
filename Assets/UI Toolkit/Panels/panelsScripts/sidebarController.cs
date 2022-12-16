0592e60780ad286e3a45acf514efe7fe93ff729f
STD\s20203050 <20203050-nakayamatoma@hcs.ac.jp>
2022-12-16 12:18:36 +0900

    サイドバーとりあえず動くようになたよ～ん

--- Assets/UI Toolkit/Panels/panelsScripts/sidebarController.cs
+++ Assets/UI Toolkit/Panels/panelsScripts/sidebarController.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class sidebarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private VisualElement sidebar;
+    private int _mainPopupIndex=-1;
+     private const string POPUP_ANIMATION = "pop_animation-hide";
+ void Awake()
+ {
+    var root=GetComponent<UIDocument>().rootVisualElement;
+    root.Q<Button>("btn").clicked+=()=>{
+        Debug.Log("メニューボタン押下");
+        sidebar=root.Q<VisualElement>("sidebar");
+        Vector2 offset=sidebar.layout.position;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+        sidebar.experimental.animation.Position(new Vector3(0,0,0),2000);
+
+    };
+ }
 }
