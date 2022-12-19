809482311947a74b63ef3efd1c51e56b1f05b80e
STD\s20203050 <20203050-nakayamatoma@hcs.ac.jp>
2022-12-19 14:45:30 +0900

    改行

--- Assets/UI Toolkit/Panels/panelsScripts/main.cs
+++ Assets/UI Toolkit/Panels/panelsScripts/main.cs
@@ -9,11 +9,15 @@ public class Main : MonoBehaviour
     private void Awake() {
          VisualElement root = GetComponent<UIDocument>().rootVisualElement;
          root.Q<Button>("back").clicked +=()=>SceneManager.LoadScene("start");
-        
+
          root.Q<Button>("shop").clicked +=()=>Debug.Log("shop");
+
          root.Q<Button>("item").clicked +=()=>Debug.Log("item");
+
          root.Q<Button>("cat-list").clicked +=()=>Debug.Log("cat-list");
+
          root.Q<Button>("setting").clicked +=()=>Debug.Log("setting");
+         
          root.Q<Button>("help").clicked +=()=>Debug.Log("help");
 
     }
