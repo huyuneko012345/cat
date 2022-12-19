b913c2d564486b4ff16debca6b67a9717d615681
STD\s20203050 <20203050-nakayamatoma@hcs.ac.jp>
2022-12-19 09:57:56 +0900

    走る猫追加

---
+++ Assets/scripts/ItemDetaBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemDetaBase : ScriptableObject
+{
+   public List<item>itemList= new List<item>();
+}
