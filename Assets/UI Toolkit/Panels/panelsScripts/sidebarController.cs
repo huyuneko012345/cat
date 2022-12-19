2bcb8937099db14c4d612df5e21ca3fd5bda174b
STD\s20203050 <20203050-nakayamatoma@hcs.ac.jp>
2022-12-19 13:35:38 +0900

    グレー背景追加

--- Assets/UI Toolkit/Panels/panelsScripts/sidebarController.cs
+++ Assets/UI Toolkit/Panels/panelsScripts/sidebarController.cs
@@ -7,29 +7,38 @@ public class SidebarController : MonoBehaviour
 {
     private VisualElement sidebar;
 
-     private bool  flg=false;
+    private bool flg = false;
 
-     [SerializeField]public float speed=10f;
- void Awake()
- {
-    var root=GetComponent<UIDocument>().rootVisualElement;
-    root.Q<Button>("btn").clicked+=()=>{
-        Debug.Log("メニューボタン押下");
-        sidebar=root.Q<VisualElement>("sidebar");
-        Vector2 offset=sidebar.layout.position;
+    [SerializeField] public float speed = 10f;
+    void Awake()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        root.Q<Button>("btn").clicked += () =>
+        {
+            Debug.Log("メニューボタン押下");
+            sidebar = root.Q<VisualElement>("sidebar");
+            Vector2 offset = sidebar.layout.position;
+            VisualElement backgroumd = root.Q<VisualElement>("gray-background");
 
-        Vector3 pos=offset;
-        Debug.Log(pos);
-        if(flg){
-            //サイドバー戻す
-                    sidebar.experimental.animation.Position(pos,1000);
-                    flg=false;
-        }else{
-            //サイドバー表示
-                    sidebar.experimental.animation.Position(-pos,1000);
-                    flg=true;
-        }
+            Vector3 pos = offset;
+            Debug.Log(pos);
+            if (flg)
+            {
+                //サイドバー戻す
+                sidebar.experimental.animation.Position(pos, 1000);
+                backgroumd.style.display = DisplayStyle.None;
 
-    };
- }
+                flg = false;
+            }
+            else
+            {
+                //サイドバー表示
+                sidebar.experimental.animation.Position(-pos, 1000);
+                backgroumd.style.display = DisplayStyle.Flex;
+
+                flg = true;
+            }
+
+        };
+    }
 }
