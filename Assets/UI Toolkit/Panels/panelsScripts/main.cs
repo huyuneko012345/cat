a9e58670bf0447265140e543cd8330f7cdd1400f
STD\s20203050 <20203050-nakayamatoma@hcs.ac.jp>
2022-12-19 14:39:42 +0900

    サイドバー押したらログでるようにした

--- Assets/UI Toolkit/Panels/panelsScripts/main.cs
+++ Assets/UI Toolkit/Panels/panelsScripts/main.cs
@@ -9,5 +9,12 @@ public class Main : MonoBehaviour
     private void Awake() {
          VisualElement root = GetComponent<UIDocument>().rootVisualElement;
          root.Q<Button>("back").clicked +=()=>SceneManager.LoadScene("start");
+        
+         root.Q<Button>("shop").clicked +=()=>Debug.Log("shop");
+         root.Q<Button>("item").clicked +=()=>Debug.Log("item");
+         root.Q<Button>("cat-list").clicked +=()=>Debug.Log("cat-list");
+         root.Q<Button>("setting").clicked +=()=>Debug.Log("setting");
+         root.Q<Button>("help").clicked +=()=>Debug.Log("help");
+
     }
 }
