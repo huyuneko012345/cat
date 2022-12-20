f26094b1d0503d3e8450b776457058d37dde6ccc
STD\s20203050 <20203050-nakayamatoma@hcs.ac.jp>
2022-12-20 14:57:28 +0900

    大きさ変更される

--- Assets/test/DataListController.cs
+++ Assets/test/DataListController.cs
@@ -6,7 +6,7 @@ using UnityEngine.UIElements;
 public class DataListController
 {
     VisualTreeAsset m_ListEntryTemplate;
-    ListView m_DataList;
+    VisualElement m_DataList;
     Label m_itemName;
     Label m_text;
     public void InitializeCharacterList(VisualElement root, VisualTreeAsset listElementTemplate)
@@ -16,11 +16,42 @@ public class DataListController
         m_ListEntryTemplate = listElementTemplate;
 
         // Store a reference to the character list element
-        m_DataList = root.Q<ListView>("list");
+        
+        m_DataList = root.Q<VisualElement>("list");
+        
+        m_DataList.Clear();
         Debug.Log(m_DataList);
+        float screenWidthPer = 0.5f;
+        float screenWidth = 0f;
+        
+
+        float screenHeightPer = 10f;
+        float screenHeight = 0f;
+        
+        m_DataList.RegisterCallback<GeometryChangedEvent>(v=>{
+            screenWidth=m_DataList.contentRect.width;
+            screenHeight=m_DataList.contentRect.height;
+            
+            Button button = new Button();
+            button.name="a"
+            button.text = "item" + 1;
+            button.style.backgroundColor = Color.red;
+            button.style.width = screenWidth*0.4f ;
+            button.style.height = screenHeight*0.2f ;
+
+
+            m_DataList.Add(button);
+
+        
+            
+        });
+        
+        float padding = (1 - screenWidthPer)/2;
+       
+
         // Store references to the selected character info elements
 
-        FillDataList();
+        // FillDataList();
 
 
         // Register to get a callback when an item is selected
@@ -33,25 +64,25 @@ public class DataListController
         m_AllDatas.AddRange(Resources.LoadAll<ItemData>("Data"));
         Debug.Log(m_AllDatas);
     }
-    void FillDataList()
-    {
-        m_DataList.makeItem = () =>
-        {
-            Debug.Log(40);
-            var newDataEntry = m_ListEntryTemplate.Instantiate();
-            var newListEntryLogic = new DataListEntryController();
-            newDataEntry.userData = newListEntryLogic;
-            Debug.Log(44);
-            newListEntryLogic.SetVisualElement(newDataEntry);
-            return newDataEntry;
-        };
-        m_DataList.bindItem = (item, i) =>
-        {
-            (item.userData as DataListEntryController).SetrData(m_AllDatas[i]);
+    // void FillDataList()
+    // {
+    //     m_DataList.makeItem = () =>
+    //     {
+    //         Debug.Log(40);
+    //         var newDataEntry = m_ListEntryTemplate.Instantiate();
+    //         var newListEntryLogic = new DataListEntryController();
+    //         newDataEntry.userData = newListEntryLogic;
+    //         Debug.Log(44);
+    //         newListEntryLogic.SetVisualElement(newDataEntry);
+    //         return newDataEntry;
+    //     };
+    //     m_DataList.bindItem = (item, i) =>
+    //     {
+    //         (item.userData as DataListEntryController).SetrData(m_AllDatas[i]);
 
-        };
-        m_DataList.fixedItemHeight = 100;
-        m_DataList.itemsSource = m_AllDatas;
+    //     };
+    //     m_DataList.fixedItemHeight = 100;
+    //     m_DataList.itemsSource = m_AllDatas;
 
-    }
+    // }
 }
