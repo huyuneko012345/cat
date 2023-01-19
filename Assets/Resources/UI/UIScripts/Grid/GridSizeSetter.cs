848dfad1315de3f1f8bbaea7e31527ee1f3c2095
STD\s20203050 <20203050-nakayamatoma@hcs.ac.jp>
2023-01-19 11:14:41 +0900

    gird修正完了

---
+++ Assets/Resources/UI/UIScripts/Grid/GridSizeSetter.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using static GridUtils;
+
+[ExecuteAlways]
+public class GridSizeSetter : MonoBehaviour
+{
+    private const int MINNUM = 0;
+    private const int MAXNUM = 1;
+
+    [Range(MINNUM, MAXNUM)][SerializeField] private float CellWidthPer = 0.9f;
+    [Range(MINNUM, MAXNUM)][SerializeField] private float CellHeightPer = 0.3f;
+    [Range(MINNUM, MAXNUM)][SerializeField] private float spacingWidthPer = 0.1f;
+    [Range(MINNUM, MAXNUM)][SerializeField] private float spacingHeightPer = 0.2f;
+
+    [Range(MINNUM, MAXNUM)][SerializeField] private float paddingTopPer = 0.2f;
+    [Range(MINNUM, MAXNUM)][SerializeField] private float paddingButtomPer = 0.2f;
+
+
+    private float CellHeight
+    {
+        get
+        {
+            return (float)(contentHeight * CellHeightPer);
+        }
+    }
+
+
+    private float CellWidth
+    {
+        get
+        {
+            return (float)(contentWidth * (CellWidthPer / 2.0));
+
+        }
+    }
+
+    private float SpacingHeight
+    {
+        get
+        {
+            return (float)(contentHeight * spacingHeightPer);
+        }
+    }
+
+    private float SpacingWidth
+    {
+        get
+        {
+            return (float)(contentWidth * spacingWidthPer);
+
+        }
+    }
+
+
+    private GameObject viewport =GameObject.Find(GridUtils.viewport);
+    private RectTransform rectTransform;
+    private GridLayoutGroup gridLayout;
+
+
+
+    private float contentHeight
+    {
+        get
+        {
+            return (float)rectTransform.rect.height;
+        }
+    }
+
+    private float contentWidth
+    {
+        get
+        {
+            return (float)rectTransform.rect.width;
+        }
+    }
+
+    private int PaddingTop
+    {
+        get
+        {
+            return (int)(contentHeight * paddingTopPer);
+        }
+    }
+
+    private int PaddingButtom
+    {
+        get
+        {
+            return (int)(contentHeight * paddingButtomPer);
+        }
+    }
+
+    void OnValidate()
+    {
+        rectTransform=viewport.GetComponent<RectTransform>();
+        gridLayout = GetComponent<GridLayoutGroup>();
+        UpdateCellSize();
+        UpdatespacingSize();
+        UpdatePaddingBottom();
+        UpdatePaddingTop();
+
+
+    }
+
+
+    private void UpdateCellSize()
+    {
+        gridLayout.cellSize = new Vector2(CellWidth, CellHeight);
+    }
+
+    private void UpdatespacingSize()
+    {
+        Debug.Log(spacingHeightPer);
+        Debug.Log(SpacingHeight);
+        gridLayout.spacing = new Vector2(SpacingWidth, SpacingHeight);
+    }
+
+    private void UpdatePaddingTop(){
+        gridLayout.padding.top=PaddingTop;
+    }
+    private void UpdatePaddingBottom(){
+        gridLayout.padding.bottom=PaddingButtom;
+    }
+
+
+
+
+}
---
+++ Assets/Resources/UI/UIScripts/Grid/GridUtils.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+
+public sealed class GridUtils
+{
+public static readonly string viewport="Viewport";
+
+
+}
