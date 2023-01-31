f3c4321f725d1127e81f3b1e6f4f717859fb2ad5
kou842 <20203052-uematsukosei@hcs.ac.jp>
2023-01-31 09:56:15 +0900

    ARの修正

---
+++ Assets/Script/PutOnCat.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PutOnCat : MonoBehaviour
+{
+    public GameObject catPrefab;
+    GameObject spawnedObj;
+    ARRaycastManager aRRaycastManager;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        aRRaycastManager = GetComponent<ARRaycastManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            if (aRRaycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose hitPose = hits[0].pose;
+                if(spawnedObj == null)
+                {
+                    spawnedObj = Instantiate(catPrefab, hitPose.position, hitPose.rotation);
+                }
+                else
+                {
+                    spawnedObj.transform.position = hitPose.position;
+
+                }
+            }
+        }
+        
+    }
+}
