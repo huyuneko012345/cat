4c7bd5c7a205ff98db06add4b7edfcde7bb99d99
STD\s20203050 <20203050-nakayamatoma@hcs.ac.jp>
2023-01-31 13:55:24 +0900

    自動的に作成してくれる

--- Assets/Resources/DB/Script/MyItem/MyItemDB.cs
+++ Assets/Resources/DB/Script/MyItem/MyItemDB.cs
@@ -7,4 +7,11 @@ public class MyItemDB : ScriptableObject
 {
     public List<MyItem>myItemList=new List<MyItem>();
 
+    public void ClearItem(){
+       myItemList.Clear();
+    }
+    public void AddItem(MyItem myItem){
+        myItemList.Add(myItem);
+    }
+
 }
--- Assets/test/test.cs
+++ Assets/test/test.cs
@@ -15,28 +15,57 @@ public class test
     {
         ItemMasterData ItemMasterData = (ItemMasterData)Resources.Load("DB/ItemMasterData");
 
-        List<Item> ItemList=new List<Item>(ItemMasterData.GetItemList());
-        List<Item> foodList=ItemList.FindAll(Item=>Item.typeId==1);
-        List<Item> toyList=ItemList.FindAll(Item=>Item.typeId==2);
-        List<Item> objList=ItemList.FindAll(Item=>Item.typeId==3);
-        CreateMyItem(foodList,"餌");
-        CreateMyItem(toyList,"おもちゃ");
-        CreateMyItem(objList,"設置物");
-        
-       
-           
+        List<Item> ItemList = new List<Item>(ItemMasterData.GetItemList());
+        List<Item> foodList = ItemList.FindAll(Item => Item.typeId == 1);
+        List<Item> toyList = ItemList.FindAll(Item => Item.typeId == 2);
+        List<Item> objList = ItemList.FindAll(Item => Item.typeId == 3);
+        CreateMyItem(foodList, "餌");
+        CreateMyItem(toyList, "おもちゃ");
+        CreateMyItem(objList, "設置物");
     }
-    private static void CreateMyItem(List<Item> itemList,string dName){
-        foreach(var item in itemList){
+    private static void CreateMyItem(List<Item> itemList, string dName)
+    {
+        foreach (var item in itemList)
+        {
             var obj = ScriptableObject.CreateInstance<MyItem>();
             obj.item = item;
             obj.count = 0;
             string fileName = $"{obj.item.name}.asset";
             var path = $"Assets/Resources/DB/MyItem/{dName}";
-            if(!Directory.Exists(path)){
+            if (!Directory.Exists(path))
+            {
                 Directory.CreateDirectory(path);
             }
-            AssetDatabase.CreateAsset(obj,Path.Combine(path,fileName));
+            AssetDatabase.CreateAsset(obj, Path.Combine(path, fileName));
         }
     }
+
+    private static MyItemDB myItemDB;
+    [MenuItem("MyGenerator/SetMyItem")]
+    private static void SetMyItem()
+    {
+
+        myItemDB = (MyItemDB)Resources.Load("DB/MyItemDB");
+        myItemDB.ClearItem();
+        Set("餌");
+        Set("おもちゃ");
+        Set("設置物");
+
+    }
+
+    private static void Set(string dirName)
+    {
+        DirectoryInfo di = new DirectoryInfo($"Assets\\Resources\\DB\\MyItem\\{dirName}");
+        Debug.Log(di);
+        FileInfo[] fileInfos = di.GetFiles("*.asset", SearchOption.AllDirectories);
+        foreach (var f in fileInfos)
+        {
+
+            var fileName = Path.GetFileNameWithoutExtension(f.ToString());
+            var a = (MyItem)Resources.Load($"DB/MyItem/{dirName}/{fileName}");
+            myItemDB.AddItem(a);
+            Debug.Log(fileName);
+        }
+    }
+
 }
