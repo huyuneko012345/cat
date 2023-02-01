18caad3370bd9517ceabc36a3e59a95bae642876
kou842 <20203052-uematsukosei@hcs.ac.jp>
2023-02-01 11:15:19 +0900

    BGMSE設定まじきもすぎ

--- Assets/Script/ChangeSoundVolume.cs
+++ Assets/Script/ChangeSoundVolume.cs
@@ -1,25 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
-// "AudioSource"コンポーネントがアタッチされていない場合アタッチ
-[RequireComponent(typeof(AudioSource))]
 public class ChangeSoundVolume : MonoBehaviour
 {
-	private AudioSource audioSource;
+	public AudioMixer audioMixer;
+	public Slider bgmSlider;
+	public Slider seSlider;
 
 	private void Start()
 	{
-		// "AudioSource"コンポーネントを取得
-		audioSource = gameObject.GetComponent<AudioSource>();
+		Debug.Log(audioMixer);
+		audioMixer.GetFloat("BGM_Volume", out float bgmVolume);
+		bgmSlider.value = bgmVolume;
+		audioMixer.GetFloat("SE_Volume", out float seVolume);
+		seSlider.value = seVolume;
 
 	}
-
-	/// <summary>
-	/// スライドバー値の変更イベント
-	/// </summary>
-	/// <param name="newSliderValue">スライドバーの値(自動的に引数に値が入る)</param>
-	public void SoundSliderOnValueChange(float newSliderValue)
+	public void SetBGM(float volume)
 	{
-		// 音楽の音量をスライドバーの値に変更
-		audioSource.volume = newSliderValue;
+		audioMixer.SetFloat("BGM_Volume", volume);
+	}
+	public void SetSE(float volume)
+	{
+		audioMixer.SetFloat("SE_Volume", volume);
 	}
 }
