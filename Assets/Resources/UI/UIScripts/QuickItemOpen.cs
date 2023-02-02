51a9b7644a24b37f635bb49d00cd0f44f7f11d6b
STD\s20203050 <20203050-nakayamatoma@hcs.ac.jp>
2023-02-02 11:38:45 +0900

    クイックアイテムを消したり表示することが出来る

--- Assets/Resources/UI/UIScripts/QuickItemOpen.cs
+++ Assets/Resources/UI/UIScripts/QuickItemOpen.cs
@@ -1,71 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Di=System.Diagnostics;
+using UnityEngine.UI;
+using Di = System.Diagnostics;
 using System;
+
 
 public class QuickItemOpen : MonoBehaviour
 {
     Di.Stopwatch sw;
-    
-    TimeSpan ts; 
+
+    TimeSpan ts;
     private Vector3 clickPosition;
 
-    /*[NonSerialized]*/public GameObject _prefab;
+    /*[NonSerialized]*/
+    public GameObject _prefab;
 
     public GameObject childButton;
     public Transform parent;
 
-    [SerializeField]private RectTransform rectTransform;
+    [SerializeField] private RectTransform rectTransform;
 
-    [SerializeField]private MyItemDB myItemDB;
+    [SerializeField] private Image image;
+
+    [SerializeField] private MyItemDB myItemDB;
 
 
 
-    private bool flg=false;
-    private bool isOpen=false;
-public void PushDown(){
-    
-    sw.Start();    
+    private bool flg = false;
+    private bool isOpen = false;
+    public void PushDown()
+    {
 
-}
-public void PushUp(){
-    Debug.Log("pushup");
-    sw.Stop();
-    ts=sw.Elapsed;
-    if(!isSec()){
-        return;
+        sw.Start();
+
     }
-    if(!isOpen){
-        Debug.Log("open");
-        List<MyItem> myItems=myItemDB.myItemList;
-    foreach(MyItem myItem in myItems){
-        Transform childTransform =(Transform)Instantiate(childButton).transform;
-        childTransform.SetParent(parent,false);
-        childTransform.GetComponent<QuickItemData>().setMyItem(myItem.item.id);
-    }
-            isOpen=true;
-
-    }else{
-        Debug.Log("close");
-        var quickItemButtons=GameObject.FindGameObjectsWithTag("QuickChildButton");
-        foreach(var Button in quickItemButtons){
-            Destroy(Button);
+    public void PushUp()
+    {
+        Debug.Log("pushup");
+        sw.Stop();
+        ts = sw.Elapsed;
+        if (!isSec())
+        {
+            return;
         }
-        isOpen=false;
-    }
-}
+        if (!isOpen)
+        {
+            Debug.Log("open");
+            List<MyItem> myItems = myItemDB.myItemList;
+            foreach (MyItem myItem in myItems)
+            {
+                Transform childTransform = (Transform)Instantiate(childButton).transform;
+                childTransform.SetParent(parent, false);
+                childTransform.GetComponent<QuickItemData>().setMyItem(myItem.item.id);
+            }
+            rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x * 6, rectTransform.sizeDelta.y);
+            image.color=new Color32(255,255,255,90);
+            isOpen = true;
 
-void Awake()
-{
-    sw=new Di.Stopwatch();
-}
-
-public  bool isSec(){
-    if(ts.Seconds>=1){
-    return true;
+        }
+        else
+        {
+            Debug.Log("close");
+            var quickItemButtons = GameObject.FindGameObjectsWithTag("QuickChildButton");
+            foreach (var Button in quickItemButtons)
+            {
+                Destroy(Button);
+            }
+            rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x / 6, rectTransform.sizeDelta.y);
+            image.color=new Color32(255,255,255,0);
+            isOpen = false;
+        }
     }
-    return false;
-}
+
+    void Awake()
+    {
+        sw = new Di.Stopwatch();
+    }
+
+    public bool isSec()
+    {
+        if (ts.Seconds >= 1)
+        {
+            return true;
+        }
+        return false;
+    }
 }
 
