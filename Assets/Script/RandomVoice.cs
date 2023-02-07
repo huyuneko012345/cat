8693f6d3f426008fb38390b46bb30e537cb364c7
kou842 <20203052-uematsukosei@hcs.ac.jp>
2023-02-07 13:39:07 +0900

    平面検出なんてなかった

--- Assets/Script/RandomVoice.cs
+++ Assets/Script/RandomVoice.cs
@@ -10,7 +10,7 @@ public class RandomVoice : MonoBehaviour
     [SerializeField] AudioClip clip3;
     [SerializeField] AudioClip clip4;
 
-    
+
 
     int rundom = 0;
     int randomidle = 0;
@@ -26,42 +26,43 @@ public class RandomVoice : MonoBehaviour
 
         //Voiceの抽選
         rundom = r1.Next(1, 4);//1~4の乱数
-        print("Voice番号："+rundom);
+        print("Voice番号：" + rundom);
 
         switch (rundom)
         {
             case 1:
-            print(rundom+"きちゃ");
+                Debug.Log(rundom + "きちゃ");
                 AudioSource.PlayOneShot(clip1);
 
-                INTERVA_SECONDS = r2.Next(3,6);
+                INTERVA_SECONDS = r2.Next(3, 6);
                 break;
 
             case 2:
-            print(rundom+"きちゃ");
+                Debug.Log(rundom + "きちゃ");
                 AudioSource.PlayOneShot(clip2);
 
-                INTERVA_SECONDS = r2.Next(3,6);
+                INTERVA_SECONDS = r2.Next(3, 6);
                 break;
 
 
             case 3:
-            print(rundom+"きちゃ");
+                Debug.Log(rundom + "きちゃ");
+
                 AudioSource.PlayOneShot(clip3);
 
-                INTERVA_SECONDS = r2.Next(3,6);
+                INTERVA_SECONDS = r2.Next(3, 6);
                 break;
 
 
             case 4:
-            print(rundom+"きちゃ");
+                Debug.Log(rundom + "きちゃ");
                 AudioSource.PlayOneShot(clip4);
 
-                INTERVA_SECONDS = r2.Next(3,6);
+                INTERVA_SECONDS = r2.Next(3, 6);
                 break;
         }
         CancelInvoke();
-        print(INTERVA_SECONDS+"秒待機");
+        Debug.Log(INTERVA_SECONDS + "秒待機");
         Invoke("catVoice", INTERVA_SECONDS);
 
     }
@@ -72,5 +73,5 @@ public class RandomVoice : MonoBehaviour
         catVoice();
     }
 
-    
+
 }
