43e113de62dc6ec7eb7b05be680e51d3d1fc5abc
STD\s20203035 <20203035-kanayakazuki@hcs.ac.jp>
2023-02-07 10:25:04 +0900

    猫餌食うです

---
+++ Assets/catstatushchange.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class catstatushchange : MonoBehaviour
+{
+
+    private DateTime nowTime;
+    public CatDataBase catdb;
+
+    void Start()
+    {
+        DateTime nowTime = DateTime.Now;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     DateTime nextTime = DateTime.Now;
+     Console.WriteLine("時:"+nextTime.Hour);
+     Console.WriteLine("時:"+nowTime.Hour);
+     int diffelence = nowTime.Hour - nextTime.Hour;
+     if (diffelence > 0){
+        //catdb.catDataList[0].
+
+     }
+    }
+}
