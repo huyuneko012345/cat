7ee3be10498e8a8c2eb5f53870cb28b47575750c
STD\s20203050 <20203050-nakayamatoma@hcs.ac.jp>
2023-02-08 09:35:41 +0900

    猫タッチで詳細がでるでる

--- Assets/Script/DetailController.cs
+++ Assets/Script/DetailController.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class DetailController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    private bool detailFlg = false;
+    [SerializeField]private GameObject DetailCanvas;
     // Update is called once per frame
+    void Awake()
+    {
+        if(this.DetailCanvas==null){
+            DetailCanvas=(GameObject)Resources.Load("UI/prefabs/DetailCanvas");
+        }
+    }
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (detailFlg)
+            {
+                detailFlg = false;
+            }
+            else
+            {
+                detailFlg = true;
+            }
+            this.DetailCanvas.SetActive(detailFlg);
+
+        }
     }
 }
