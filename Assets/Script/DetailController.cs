20919d7d456aecec44d5e36c7a44d04c2f48142c
STD\s20203050 <20203050-nakayamatoma@hcs.ac.jp>
2023-02-08 10:18:22 +0900

    猫クリック詳細の表示きりかえ

--- Assets/Script/DetailController.cs
+++ Assets/Script/DetailController.cs
@@ -1,34 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DetailController : MonoBehaviour
+public class DetailController : MonoBehaviour, IPointerDownHandler
 {
     // Start is called before the first frame update
 
     private bool detailFlg = false;
-    [SerializeField]private GameObject DetailCanvas;
+    [SerializeField] private GameObject DetailCanvas;
     // Update is called once per frame
     void Awake()
     {
-        if(this.DetailCanvas==null){
-            DetailCanvas=(GameObject)Resources.Load("UI/prefabs/DetailCanvas");
+        if (this.DetailCanvas == null)
+        {
+            DetailCanvas = (GameObject)Resources.Load("UI/prefabs/DetailCanvas");
         }
     }
-    void Update()
+    // クリック
+    public void OnPointerDown(PointerEventData pointerEventData)
     {
-        if (Input.GetMouseButtonDown(0))
+        
+        if (detailFlg)
         {
-            if (detailFlg)
-            {
-                detailFlg = false;
-            }
-            else
-            {
-                detailFlg = true;
-            }
-            this.DetailCanvas.SetActive(detailFlg);
-
+            detailFlg = false;
         }
+        else
+        {
+            detailFlg = true;
+        }
+        this.DetailCanvas.SetActive(detailFlg);
+
     }
 }
