611dee10aaeee4fd7006e0bd190e69a2f151078f
kou842 <20203052-uematsukosei@hcs.ac.jp>
2023-02-10 11:35:03 +0900

    鳴き声こめこめ

--- Assets/Script/RandomVoice.cs
+++ Assets/Script/RandomVoice.cs
@@ -2,77 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 猫の鳴き声を配列内からランダムで出力するクラス
+/// </summary>
 public class RandomVoice : MonoBehaviour
 {
+    //スクリーン内で音を出すための変数
     public AudioSource AudioSource;
+    //鳴き声のファイルの配列。現在４つの鳴き声を実装
     [SerializeField] AudioClip clip1;
     [SerializeField] AudioClip clip2;
     [SerializeField] AudioClip clip3;
     [SerializeField] AudioClip clip4;
 
-    
+    //
+    int voice_rundom = 0;
+    //ランダム関数の呼び出し(音声用)
+    System.Random voice_set = new System.Random();
+    //ランダム関数の呼び出し(鳴き声のインターバル時間)
+    System.Random interval = new System.Random();
 
-    int rundom = 0;
-    int randomidle = 0;
-    System.Random r1 = new System.Random();
-    System.Random r2 = new System.Random();
-
+    //インターバル時間の初期値
     private float INTERVA_SECONDS = 10f;
 
 
-    //猫の鳴き声を不定期に出すメソッド
+    /// <summary>
+    /// 鳴き声を配列からランダムで出力するメソッド
+    /// 現在４つ作成
+    /// </summary>
     public void catVoice()
     {
 
         //Voiceの抽選
-        rundom = r1.Next(1, 4);//1~4の乱数
-        print("Voice番号："+rundom);
+        voice_rundom = voice_set.Next(1, 4);//1~4の乱数
+        print("Voice番号：" + voice_rundom);
 
-        switch (rundom)
+        switch (voice_rundom)
         {
             case 1:
-            print(rundom+"きちゃ");
+                print(voice_rundom + "きちゃ");
                 AudioSource.PlayOneShot(clip1);
 
-                INTERVA_SECONDS = r2.Next(3,6);
+                INTERVA_SECONDS = interval.Next(3, 6);
                 break;
 
             case 2:
-            print(rundom+"きちゃ");
+                print(voice_rundom + "きちゃ");
                 AudioSource.PlayOneShot(clip2);
 
-                INTERVA_SECONDS = r2.Next(3,6);
+                INTERVA_SECONDS = interval.Next(3, 6);
                 break;
 
 
             case 3:
-            print(rundom+"きちゃ");
+                print(voice_rundom + "きちゃ");
                 AudioSource.PlayOneShot(clip3);
 
-                INTERVA_SECONDS = r2.Next(3,6);
+                INTERVA_SECONDS = interval.Next(3, 6);
                 break;
 
 
             case 4:
-            print(rundom+"きちゃ");
+                print(voice_rundom + "きちゃ");
                 AudioSource.PlayOneShot(clip4);
 
-                INTERVA_SECONDS = r2.Next(3,6);
+                INTERVA_SECONDS = interval.Next(3, 6);
                 break;
         }
+
         CancelInvoke();
-        print(INTERVA_SECONDS+"秒待機");
+        //インターバル後に上記の処理を繰り返す
         Invoke("catVoice", INTERVA_SECONDS);
 
     }
 
     void Start()
     {
-        Debug.Log("Voice実行");
         catVoice();
-    }
-
-    void Update(){
-        Debug.Log("案山子万歳");
     }
 }
